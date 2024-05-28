61fd041b90cd09a9ee1c89b428b06d5e028ee92d
basit3407 <basit3407@gmail.com>
2024-05-28 20:21:08 +0500

    wip

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ import SvgIcon4 from './icons/SvgIcon4'
 import SvgIcon5 from './icons/SvgIcon5'
 import messages from './messages.json'
 import './style.css'
-import CallUsModal from './CallUsModal'
+import CallUsModal from './CallUsModal' // Import the modal component
 import { mockData } from '../../util'
 
 const NavBar = () => {
@@ -21,40 +21,40 @@ const NavBar = () => {
   }
 
   return (
-    <>
-      <div className="container">
-        <div className="flex-centered-row">
-          <img src="/assets/logo.png" className="product-thumbnail" />
-          <div className="navigation-bar">
-            <div className="horizontal-nav-container">
-              <div className="product-card-container">
-                <p className="product-info-card">{messages['products']}</p>
-                <SvgIcon1 className="svg-container" />
-              </div>
-              <p className="product-info-card">{messages['events']}</p>
-              <p className="product-info-card">{messages['about_us']}</p>
+    <div className="container">
+      <div className="flex-centered-row">
+        <img src="/assets/logo.png" className="product-thumbnail" />
+        <div className="navigation-bar">
+          <div className="horizontal-nav-container">
+            <div className="product-card-container">
+              <p className="product-info-card">{messages['products']}</p>
+              <SvgIcon1 className="svg-container" />
             </div>
-            <div className="horizontal-flex-container-with-icons">
-              <div className="callout-container">
-                <SvgIcon2 className="svg-container1" />
+            <p className="product-info-card">{messages['events']}</p>
+            <p className="product-info-card">{messages['about_us']}</p>
+          </div>
+          <div className="horizontal-flex-container-with-icons">
+            <div className="callout-container">
+              <SvgIcon2 className="svg-container1" />
+            </div>
+            <p className="product-info-card" onClick={handleCallUsClick}>
+              {messages['call_us']}
+            </p>
+            <div className="contact-section">
+              <div className="card-container-with-title">
+                <SvgIcon3 className="svg-container2" />
               </div>
-              <p className="product-info-card">{messages['call_us']}</p>
-              <div className="contact-section">
-                <div className="card-container-with-title">
-                  <SvgIcon3 className="svg-container2" />
-                </div>
-                <div className="card-container-with-title">
-                  <SvgIcon4 className="svg-container2" />
-                </div>
-                <div className="card-container-with-title">
-                  <SvgIcon5 className="svg-container2" />
-                </div>
+              <div className="card-container-with-title">
+                <SvgIcon4 className="svg-container2" />
+              </div>
+              <div className="card-container-with-title">
+                <SvgIcon5 className="svg-container2" />
               </div>
             </div>
           </div>
         </div>
       </div>
-      {showModal && (
+      {/* {showModal && (
         <>
           <div className="backdrop" onClick={handleCloseModal}></div>
           <div className="modal">
@@ -63,8 +63,8 @@ const NavBar = () => {
             />
           </div>
         </>
-      )}
-    </>
+      )} */}
+    </div>
   )
 }
 
